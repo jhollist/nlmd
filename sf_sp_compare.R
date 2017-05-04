14a4493e70c65843c131e00abe2e28ccd70ec1eb
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2017-05-04 09:22:35 -0400

    updates to build_db.  Need to re-calc uncorrected depths

--- sf_sp_compare.R
+++ sf_sp_compare.R
@@ -1,8 +1,6 @@
 library(httr)
 library(sf)
 library(dplyr)
-library(elevatr)
-library(lakemorpho)
 
 if(!file.exists("data/lakemorphometry.zip")){
   GET("https://edg.epa.gov/data/PUBLIC/ORD/NHEERL/LakeMorphometry.zip",
@@ -12,18 +10,28 @@ if(!dir.exists("data/LakeMorphGdb.gdb/")){
   unzip("data/lakemorphometry.zip",exdir = "data/")
 }
 lyrs <- st_layers("data/LakeMorphGdb.gdb/")$name
+sf_time <- system.time({
 all_morpho <- assign(lyrs[1],st_read("data/LakeMorphGdb.gdb/",lyrs[1],
                                      stringsAsFactors = FALSE)) %>%
   mutate(huc_region = lyrs[1])
 for(i in lyrs[-1]){
   assign(i, st_read("data/LakeMorphGdb.gdb/",i,stringsAsFactors = FALSE))
   all_morpho <- get(i) %>%
+    #filter(nlaSITE_ID != "NA") %>%
     mutate(huc_region = i) %>%
     rbind(all_morpho)
 }
+})
 
-nla_morpho <- all_morpho %>%
-  filter(!is.na(nlaSITE_ID))
-         
-for(i in seq_along(nla_morpho))
-  
+library(sp)
+library(rgdal)
+library(rgeos)
+sp_time <- system.time({
+all_morpho_sp <- assign(lyrs[1],readOGR(dsn = "data/LakeMorphGdb.gdb/", layer = lyrs[1]))
+for(i in lyrs[-1]){
+  assign(i,readOGR(dsn = "data/LakeMorphGdb.gdb/", layer = i))
+  all_morpho_sp <- gUnion(all_morpho_sp, get(i))
+}
+})
+
+
